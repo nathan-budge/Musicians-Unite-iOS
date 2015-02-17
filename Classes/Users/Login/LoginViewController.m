98d56899f1127f5dd8d1f564cdd193469afa89d6
nbudge <nab75@case.edu>
2015-02-17 08:43:24 -0500

    Added Login Functionality

--- Classes/Users/Login/LoginViewController.m
+++ Classes/Users/Login/LoginViewController.m
@@ -6,27 +6,201 @@
 //  Copyright (c) 2015 CWRU. All rights reserved.
 //
 
+#import <Firebase/Firebase.h>
+#import "SVProgressHUD.h"
+#import "AppConstant.h"
+
 #import "LoginViewController.h"
+
 
 @interface LoginViewController ()
 
+//Firebase Reference
+@property (nonatomic) Firebase *ref;
+
+//Text Fields
+@property (weak, nonatomic) IBOutlet UITextField *fieldEmail;
+@property (weak, nonatomic) IBOutlet UITextField *fieldPassword;
+
 @end
+
 
 @implementation LoginViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view.
+#pragma mark - Lazy instantiation
+
+- (Firebase *)ref
+{
+    if (!_ref) {
+        _ref = [[Firebase alloc] initWithUrl:FIREBASE_URL];
+    }
+    
+    return _ref;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+#pragma mark - View handling
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    //Add tap gesture for removing the keyboard
+    [self.view addGestureRecognizer:[[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(dismissKeyboard)]];
+    
 }
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    [self.fieldEmail becomeFirstResponder];
+}
+
+-(void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+    [self dismissKeyboard];
+}
+
+
+#pragma mark - Status bar color
 
 -(UIStatusBarStyle)preferredStatusBarStyle{
     return UIStatusBarStyleLightContent;
 }
+
+
+#pragma mark - Buttons
+
+- (IBAction)actionLogin:(id)sender {
+    
+    
+    [SVProgressHUD showWithStatus:@"Logging in..." maskType:SVProgressHUDMaskTypeBlack];
+    [self dismissKeyboard];
+    
+    [self.ref authUser:self.fieldEmail.text password:self.fieldPassword.text withCompletionBlock:^(NSError *error, FAuthData *authData) {
+        
+        if (error) {
+            switch(error.code) {
+                case FAuthenticationErrorInvalidEmail:
+                    [SVProgressHUD showErrorWithStatus:@"Invalid email and/or password"];
+                    self.fieldPassword.text = @"";
+                    break;
+                case FAuthenticationErrorInvalidPassword:
+                    [SVProgressHUD showErrorWithStatus:@"Invalid email and/or password"];
+                    self.fieldPassword.text = @"";
+                    break;
+                case FAuthenticationErrorUserDoesNotExist:
+                    [SVProgressHUD showErrorWithStatus:@"User does not exist"];
+                    self.fieldPassword.text = @"";
+                    break;
+                case FAuthenticationErrorNetworkError:
+                    [SVProgressHUD showErrorWithStatus:@"Network Error"];
+                    self.fieldPassword.text = @"";
+                    break;
+                default:
+                    [SVProgressHUD showErrorWithStatus:error.description];
+                    self.fieldPassword.text = @"";
+                    break;
+            }
+            
+        } else {
+            
+            [self performSegueWithIdentifier:@"LoginToGroups" sender:sender];
+        }
+    }];
+    
+}
+
+
+- (IBAction)actionTogglePasswordVisibility:(id)sender{
+    
+    if ([sender isSelected]) {
+        [sender setImage:[UIImage imageNamed:@"eye_inactive"] forState:UIControlStateNormal];
+        self.fieldPassword.secureTextEntry = YES;
+        [sender setSelected:NO];
+    } else {
+        [sender setImage:[UIImage imageNamed:@"eye_active"] forState:UIControlStateSelected];
+        self.fieldPassword.secureTextEntry = NO;
+        [sender setSelected:YES];
+    }
+}
+
+
+- (IBAction)actionForgotPassword:(id)sender {
+    [self.fieldEmail resignFirstResponder];
+    [self.fieldPassword resignFirstResponder];
+    
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Please enter an email address" message:nil delegate:self cancelButtonTitle:@"Cancel" otherButtonTitles:@"OK", nil];
+    alert.alertViewStyle = UIAlertViewStylePlainTextInput;
+    [alert show];
+    
+}
+
+#pragma mark - Forgot Password Alert View
+
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    if (buttonIndex != alertView.cancelButtonIndex)
+    {
+        if ([alertView textFieldAtIndex:0]) {
+            
+            UITextField *textField = [alertView textFieldAtIndex:0];
+            
+            if (![textField.text isEqualToString:@""] && ![self validateEmail:textField.text])
+            {
+                [SVProgressHUD showErrorWithStatus:@"Invalid email"];
+            }
+            
+            else {
+                
+                [self.ref resetPasswordForUser:textField.text withCompletionBlock:^(NSError *error) {
+                    
+                    if (error) {
+                        NSLog(@"%@", error.description);
+                    }
+                    
+                    else {
+                        [SVProgressHUD showSuccessWithStatus:@"Email sent!"];
+                        
+                    }
+                
+                }];
+            
+            }
+        }
+    }
+}
+
+
+- (BOOL)validateEmail:(NSString *)emailStr {
+    NSString *emailRegex = @"[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}";
+    NSPredicate *emailTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", emailRegex];
+    return [emailTest evaluateWithObject:emailStr];
+}
+
+
+
+#pragma mark - Keyboard handling
+
+-(void)dismissKeyboard
+{
+    [self.view endEditing:YES];
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    if (textField == self.fieldEmail)
+    {
+        [self.fieldPassword becomeFirstResponder];
+    }
+    if (textField == self.fieldPassword)
+    {
+        [self dismissKeyboard];
+    }
+    return YES;
+}
+
+
 
 /*
 #pragma mark - Navigation
